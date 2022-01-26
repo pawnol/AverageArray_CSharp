0c43e17b8b85b68822fc489d2920124b8c5214f6
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2022-01-25 21:32:25 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Average Array
+ * Pawelski
+ * 1/25/2022
+ * Read over the code and run the program a few times to determine what
+ * the program does. Once you know what the program does and how it works,
+ * answer the questions on the activity sheet.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0, average;
+            double[] numbers = { 5.6, 77, 5.3, 8.49, 50.1, 66.2 };
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            average = sum / numbers.Length;
+            Console.WriteLine("The average is " + average);
+        }
+    }
+}
